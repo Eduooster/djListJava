424749e7a26f78266ce36af0c6729b4fffb0c09e
Seu Nome <edudz1404@gmail.com>
2025-05-19 14:18:31 -0300

    Project created

---
+++ src/test/java/com/example/dsgamelist/DsGameListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsgamelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsGameListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
